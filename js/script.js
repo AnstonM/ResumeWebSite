07029b3a9844cbbc7f3cb9ff0b23bdc8a46caf56
AnstonM <87513070+AnstonM@users.noreply.github.com>
2021-08-31 20:32:47 +0530

    8.32

--- js/script.js
+++ js/script.js
@@ -12,16 +12,23 @@ $(function () {
   });
 });
 
-   $("#myCarousel").swiperight(function() {
-      $(this).carousel('prev');
-    });
-   $("#myCarousel").swipeleft(function() {
-      $(this).carousel('next');
-   });
+$('.carousel').on('touchstart', function(event){ 
+  const xClick = event.originalEvent.touches[0].pageX; $(this).one('touchmove', 
+    function(event){ 
+      const xMove = event.originalEvent.touches[0].pageX; 
+      const sensitivityInPx = 5; 
+      if( Math.floor(xClick - xMove) > sensitivityInPx ){ 
+        $(this).carousel('next'); 
+      } 
+      else if( Math.floor(xClick - xMove) < -sensitivityInPx ){ 
+        $(this).carousel('prev'); 
+      } 
+    }); 
+  $(this).on('touchend', function()
+    { $(this).off('touchmove'); 
+  }); 
+});
 
-   $('.carousel').carousel({
-    interval: false,
-   });
 
 (function (global) {
 
